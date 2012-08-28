f389595689a639481061d43880a6bd326feb6a75
Cheng-Yu Lin <ntuaha@gmail.com>
2012-08-28 23:35:07 +0800

    add pg

--- web.js
+++ web.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var pg = require('pg');
 
 var app = express.createServer(express.logger());
 
@@ -9,4 +10,13 @@ app.get('/', function(request, response) {
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+});
+
+
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  var query = client.query('SELECT * FROM your_table');
+
+  query.on('row', function(row) {
+    console.log(JSON.stringify(row));
+  });
 });
