dc026904b73c1c4fa4f6433c4bde7031abb48f39
Wush Wu <wush978@gmail.com>
2012-08-30 16:35:09 +0800

    query finish

--- src/Zhuyinwen/QueryBundle/Controller/DefaultController.php
+++ src/Zhuyinwen/QueryBundle/Controller/DefaultController.php
@@ -5,35 +5,40 @@ namespace Zhuyinwen\QueryBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Zhuyinwen\QueryBundle\Entity\Term;
 
 class DefaultController extends Controller
 {
 	/**
-	 * @Route("/")
+	 * @Route("/", name="_welcome")
 	 */
 	public function indexAction() 
 	{
-		return new Response('<html><body>Welcome!</body></html>');
+		return $this->render('ZhuyinwenQueryBundle:Default:index.html.twig');
 	}
 	
     /**
-     * @Route("/query/{zhuyin}")
+     * @Route("/query", name="_query")
+     * @Method({"POST"})
      */
-    public function queryAction($zhuyin)
+    public function queryAction()
     {
-    	$result = Term::query($zhuyin);
-    	$retval = '';
-    	if (is_null($result)) {
-    		$retval .= '查無資料';
+    	$request = $this->get('request');
+    	$query = $request->request->get('query');
+    	if ($query != "") {
+    		$query_result = Term::query($query);
+    		$retval = array(
+    				"responseCode" => 200,
+    				"message" => $query_result
+    				);
+    	} else {
+    		$retval = array(
+    				"responseCode" => 400,
+    				"message" => "錯誤的查詢值"
+    				);
     	}
-    	else {
-    		foreach($result as $result_element) {
-    			$retval .= $result_element . '<br/>';
-    		}
-    	}
-        return new Response('<html><body>' . $retval . '</body></html>');
+    	$retval = json_encode($retval);
+    	return new Response($retval, 200, array('Content-Type'=>'application/json'));
     }
-    
-    
 }
--- src/Zhuyinwen/QueryBundle/Entity/Term.php
+++ src/Zhuyinwen/QueryBundle/Entity/Term.php
@@ -9,6 +9,21 @@ class Term
 	static protected $dictionary = NULL;
 	
 	static public function query($query) {
+		$retval = self::query_internal($query);
+		if (!is_array($retval)) {
+			return "查無資料";
+		}
+		else {
+			$retval_string = '<ul>';
+			foreach($retval as $value) {
+				$retval_string .= "<li>$value</li>";
+			}
+			$retval_string .= '</ul>';
+			return $retval_string;
+		}
+	}
+	
+	static private function query_internal($query) {
 		if (is_null(self::$dictionary)) {
 			self::$dictionary = unserialize(file_get_contents(__DIR__ . self::$phpdata_path));
 		}
